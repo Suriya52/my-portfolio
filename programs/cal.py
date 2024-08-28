5634c35b2a7d86e920f02cef54033c4e25d8b416
Suriya52 <surya6379r@gmail.com>
2024-08-28 11:49:37 +0530

    Initial commit

---
+++ programs/cal.py
@@ -0,0 +1,11 @@
+#calender...
+"""import calendar 
+yy=2024
+mm=8
+print(calendar.month(yy,mm))"""
+
+import calendar
+yy=int(input("Enter the year you want:"))
+mm=int(input("Enter the month you want:"))
+print(calendar.month(yy,mm))
+
