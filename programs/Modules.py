2db604b6717cbf9ee42ccfd6e4d51c3eb93c2889
Suriya52 <surya6379r@gmail.com>
2024-08-30 13:01:01 +0530

    Adding Some more programs

--- programs/Modules.py
+++ programs/Modules.py
@@ -17,3 +17,10 @@ div(2,4) """
 # There is a Saying that goes like "when I worte this Code only God and I understood what it did...
 #                                                       And Now only God Knows...."
 
+# Modules we use our own Modules here "calc" is our own modules.
+
+from calc import *
+a=10
+b=20
+c=mul(a,b)
+print(c)
